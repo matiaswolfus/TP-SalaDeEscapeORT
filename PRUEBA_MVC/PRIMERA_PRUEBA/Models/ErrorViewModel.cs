337808a49e467485b1bb16647ec75815ddc67bf4
MATÍAS WOLFUS <49194355@ort.edu.ar>
2025-06-05 13:42:15 -0300

    Correccion SOLO TP

--- PRUEBA_MVC/PRIMERA_PRUEBA/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace PRIMERA_PRUEBA.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
