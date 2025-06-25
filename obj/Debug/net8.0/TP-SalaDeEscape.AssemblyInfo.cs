186d63dd7266da1ec5726b6963288032569c3d69
matiaswolfus <49194355@est.ort.edu.ar>
2025-06-25 11:15:33 -0700

    agregue 600 segs

--- obj/Debug/net8.0/TP-SalaDeEscape.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP-SalaDeEscape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-SalaDeEscape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03e41d565ccb3059fbef9c5a1779fa4a38600b2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e85ca2c9464de6155f19ecb2a14e4322a9bced")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-SalaDeEscape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-SalaDeEscape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
