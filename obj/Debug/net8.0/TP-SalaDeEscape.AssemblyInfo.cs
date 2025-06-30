12085d9ce75fc58e648ff8ae94085655c7c2562c
ZOE ACQUISTAPACE <49091323@ort>
2025-06-30 15:31:34 -0300

    Css fondo de index, no me dejo

--- obj/Debug/net8.0/TP-SalaDeEscape.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP-SalaDeEscape.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP-SalaDeEscape")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f4bc8605843ca1e6abb79624450b6eb73d28397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc8e7dff00d10e728f0b1060a5059c559fb4a65")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP-SalaDeEscape")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP-SalaDeEscape")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
